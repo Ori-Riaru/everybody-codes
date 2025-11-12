974b2fa83d720eca25369c4d647d3fb6a04cf6d8
Ori Riaru <ori-riaru@proton.me>
2025-11-11 19:42:17 -0500

    solve(day7): Namegraph

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,41 +1,105 @@
 use std::fs;
+use std::collections::{HashMap, HashSet};
+use std::path::Prefix;
+use itertools::Itertools;
 
-fn parse_input(path: String) -> Vec<i32> {
+fn parse_input(path: String) -> (Vec<String>, HashMap<char, Vec<char>>) {
     let input = fs::read_to_string(path).expect("Failed to read input file");
-    let inputs: Vec<i32> = input
-        .trim()
-        .split(',')
-        .map(|s| s.parse().unwrap())
+    let names: Vec<String> = input.lines().next().unwrap().split(",").map(|s| s.to_string()).collect();
+    let rules: HashMap<char, Vec<char>> = input.lines().skip(2)
+        .map(|s| {
+            let mut parts = s.split(">");
+            let key = parts.next().unwrap().chars().next().unwrap();
+            let value = parts.next().unwrap().split(",").map(|s| s.trim().chars().next().unwrap()).collect();
+
+            (key, value)
+        })
         .collect();
 
-    return inputs;
+    (names, rules)
 }
 
-fn part1(creates: Vec<i32>) -> i32 {
+fn part1(names: Vec<String>,  rules: HashMap<char, Vec<char>>) -> String {
     
-    todo!()
+    let mut result = "".to_string();
+    'outer: for name in names.iter() {
+        for (current, next) in name.chars().tuple_windows() {
+            if !rules.get(&current).unwrap().contains(&next) {
+                continue 'outer;
+            }
+        }
+
+        result = name.clone();
+        break;
+    }
+    
+    result 
 }
 
-fn part2(creates: Vec<i32>) -> i32 {
+fn part2(names: Vec<String>, rules: HashMap<char, Vec<char>>) -> (Vec<String>, i32) {
     
-    todo!()
+    let mut result = 0;
+    let mut list = Vec::new();
+    'outer: for (i,name) in names.iter().enumerate() {
+        for (current, next) in name.chars().tuple_windows() {
+            if !rules.get(&current).unwrap().contains(&next) {
+                continue 'outer;
+            }
+        }
+
+        result += i + 1;
+        list.push(name.clone());
+    }
+    
+    (list, result as i32)
 }
 
-fn part3(creates: Vec<i32>) -> i32 {
+fn part3(prefixes: Vec<String>, rules: HashMap<char, Vec<char>>) -> i32 {
+    let mut stack: Vec<String> = Vec::new();
+    let mut found: HashSet<String> = HashSet::new();
+
+    let (prefixes, _) = part2(prefixes, rules.clone());
+    for prefix in prefixes {
+        stack.push(prefix);            
+    }
+        
     
-    todo!()
+    let mut count = 0;
+    while let Some(item) = stack.pop() {
+        if found.contains(&item) {
+            continue;
+        }
+        
+        found.insert(item.clone());
+        if item.len() >= 7 {
+            count += 1;
+        }
+
+        for next in  rules.get(&item.chars().last().unwrap()).unwrap_or(&Vec::<char>::new()) {
+            let mut new = item.clone();
+            new.push(*next);
+            
+            if new.len() > 11 {
+                continue;
+            }
+
+            stack.push(new);
+        }
+    }   
+
+    count
 }
 
 fn main() {
     // Part 1
-    let inputs = parse_input("day7/test1.txt".to_string());
-    println!("Part 1: {}", part1(inputs));
+    let (names, rules) = parse_input("day7/part1.txt".to_string());
+    println!("Part 1: {}", part1(names, rules));
 
     // Part 2
-    let inputs = parse_input("day7/test2.txt".to_string());
-    println!("Part 2: {}", part2(inputs));
+    let (names, rules) = parse_input("day7/part2.txt".to_string());
+    println!("Part 2: {}", part2(names, rules).1);
 
     // Part 3
-    let inputs = parse_input("day7/test3.txt".to_string());
-    println!("Part 3: {}", part3(inputs));
+    let (names, rules) = parse_input("day7/part3.txt".to_string());
+    println!("Part 3: {}", part3(names, rules));
 }
